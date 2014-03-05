21eeeaf2ec66c3de2de9754023185fc4975fe88d
yemyat <thu.yemyat@gmail.com>
2014-03-06 02:25:48 +0630

    Just a little change

--- products/p880.mk
+++ products/p880.mk
@@ -1,5 +1,5 @@
 # Inherit CM device configuration for p880. 
-$(call inherit-product, device/lge/p880/cm.mk)
+$(call inherit-product, device/lge/p880/p880.mk)
 
 # Inherit common product files.
 $(call inherit-product, vendor/xenonhd/products/common.mk)
@@ -13,6 +13,10 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/xenonhd/overlay/toro
 # Extra tuna overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/xenonhd/overlay/tuna
 
+# This device has NFC
+$(call inherit-product, vendor/xenonhd/configs/nfc_enhanced.mk)
+
+
 # Copy toro specific prebuilt files
 PRODUCT_COPY_FILES +=  \
 	vendor/xenonhd/proprietary/hybrid/hybrid_xhdpi.conf:system/etc/beerbong/properties.conf \
@@ -23,6 +27,15 @@ PRODUCT_COPY_FILES +=  \
 
 # Inherit drm blobs
 -include vendor/xenonhd/products/common_drm.mk
+
+PRODUCT_RELEASE_NAME := Optimus4XHD
+PRODUCT_DEVICE := p880
+PRODUCT_NAME := liquid_p880
+PRODUCT_BRAND := lge
+PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=x3_open_eu BUILD_FINGERPRINT=lge/x3_open_eu/x3:4.1.2/JZO54K/P88020a.1e56bb4c69:user/release-keys PRIVATE_BUILD_DESC="x3_open_eu-user 4.1.2 JZO54K P88020a.1e56bb4c69 release-keys"
+
 
 
 
